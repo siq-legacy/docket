7629cdcb287fc5530bc2d001710d9dec04b98edd
Richard Arnold <rarnold@stordiq.com>
2013-02-21 09:03:39 -0600

    Basic package support.

---
+++ docket/migrations/versions/7c77271b19c_add_pkg_status.py
@@ -0,0 +1,20 @@
+"""add_pkg_status
+
+Revision: 7c77271b19c
+Revises: None
+Created: 2013-02-21 08:59:44.930802
+"""
+
+revision = '7c77271b19c'
+down_revision = None
+
+from alembic import op
+from spire.schema.fields import *
+from sqlalchemy import Column, ForeignKey, ForeignKeyConstraint, PrimaryKeyConstraint, CheckConstraint
+from sqlalchemy.dialects import postgresql
+
+def upgrade():
+    op.add_column('package', Column('status', EnumerationType(), nullable=True))
+
+def downgrade():
+    op.drop_column('package', 'status')
