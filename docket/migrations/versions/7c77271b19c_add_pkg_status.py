d60f076d0c48c3f5d843e78a6a191bc93eb05141
Richard Arnold <rarnold@stordiq.com>
2013-02-21 10:08:48 -0600

    Adjust down_revision.

--- docket/migrations/versions/7c77271b19c_add_pkg_status.py
+++ docket/migrations/versions/7c77271b19c_add_pkg_status.py
@@ -6,7 +6,7 @@ Created: 2013-02-21 08:59:44.930802
 """
 
 revision = '7c77271b19c'
-down_revision = None
+down_revision = '591141df4d22'
 
 from alembic import op
 from spire.schema.fields import *
